34fbf307c0ea3c3934d98d5eac5c705a67606a31
Logan Reed <me@loganreed.org>
2024-08-02 07:16:48 -0400

    Finished Sparse Stencil

--- neuropy/solver.py
+++ neuropy/solver.py
@@ -3,6 +3,8 @@ from enum import Enum
 import navis as nv
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.linalg import lu
+import collections
 
 class Consts(Enum):
     VCLAMP = 0.05
@@ -16,10 +18,7 @@ class Consts(Enum):
     NI = 0.0376969
     MI = 0.0147567
     HI = 0.9959410
-
-def normalizeSWC(filename):
-    n = nv.read_swc(filename)
-
+    SCF = 1e-6
 
 
 class Solver:
@@ -31,6 +30,15 @@ class Solver:
         # NOTE: This is required for setTargetTimeStep
         # since "excessively" small edges mess things up
         nv.resample_skeleton(self.neuron, resample_to=3.0,inplace=True)
+        # self.neighbors = nv.neuron2nx(self.neuron).adj
+        # self.neighbors = [[] for _ in range(self.neuron.nodes['node_id'].size)]
+        self.neighbors = collections.defaultdict(list)
+        # print(len(self.neighbors))
+        for e in self.neuron.edges:
+            # print(self.neighbors[e[0]])
+            self.neighbors[e[0]].append(e[1])
+            self.neighbors[e[-1]].append(e[0])
+        # print(self.neighbors)
 
         # get euclidean distances between node and its parent
         # Get nodes but remove the root (has no parent)
@@ -53,20 +61,64 @@ class Solver:
         self.h_pre = self.h
         self.isyn = np.full(self.neuron.nodes['node_id'].size, 0.0)
         self.r = np.full(self.neuron.nodes['node_id'].size, 0.0)
-        self.temp_state = np.full(self.neuron.nodes['node_id'].size, 0.0)
         self.time_step = self.setTargetTimeStep()
+        self.sparse_stencils = self.makeSparseStencils()
+        self.temp_state = np.full(self.neuron.nodes['node_id'].size, 0.0)
+
 
     def setTargetTimeStep(self):
         dtmax = 50e-6
-        scf = 1e-6
         gll = Consts.GL.value
         if gll == 0.0:
             gll = 1.0
-        upper_bound = Consts.CAP.value * self.neuron.sampling_resolution * scf * np.sqrt(Consts.RES.value / (gll * np.min(self.distances) * scf ))
+        upper_bound = Consts.CAP.value * self.neuron.sampling_resolution * Consts.SCF.value * np.sqrt(Consts.RES.value / (gll * np.min(self.distances) * Consts.SCF.value ))
+        self.time_step = min(upper_bound, dtmax)
         return min(upper_bound, dtmax)
 
     def reactF(self, u_active, n, m, h):
         return 0.1
+
+    def makeSparseStencils(self):
+        lhs = self.neuron.geodesic_matrix.astype('float64')
+        for col in lhs.columns:
+            lhs[col].values[:] = 0.0
+        lhs = lhs.astype('float64')
+        rhs = self.neuron.geodesic_matrix.astype('float64')
+        for col in rhs.columns:
+            rhs[col].values[:] = 0.0
+        rhs = rhs.astype('float64')
+
+        # lhs = np.zeros((self.neuron.nodes['node_id'].size,self.neuron.nodes['node_id'].size))
+        # rhs = np.zeros((self.neuron.nodes['node_id'].size,self.neuron.nodes['node_id'].size))
+
+        # for i in range(self.neuron.nodes['node_id'].size):
+
+        # To get distance between two values
+        # print(self.neuron.geodesic_matrix.at[self.neuron.edges[i][0], self.neuron.edges[i][1]])
+
+
+        # neighbors = [[] for _ in range(self.neuron.nodes['node_id'].size)]
+        # for e in self.neuron.edges:
+        #     neighbors[e[0]].append(e[1])
+        #     neighbors[e[1]].append(e[0])
+        for n in self.neuron.nodes['node_id']:
+            edgeLengths = []
+            sumRecip = 0.0
+            tempRadius = self.neuron.nodes.loc[self.neuron.nodes['node_id'] == n, 'radius'].values[0] * Consts.SCF.value
+            for ngh in self.neighbors[n]:
+                edgeLengths.append(self.neuron.geodesic_matrix.at[n, ngh] * Consts.SCF.value)
+                tempNeighborRadius = self.neuron.nodes.loc[self.neuron.nodes['node_id'] == ngh, 'radius'].values[0] * Consts.SCF.value
+                sumRecip = sumRecip + 1 / (edgeLengths[-1] * tempRadius * ((1 / (tempNeighborRadius * tempNeighborRadius)) + (1 / (tempRadius * tempRadius))))
+
+            avgEdgeLengths = sum(edgeLengths) / len(edgeLengths)
+            rhs.at[n,n] = 1.0
+            lhs.at[n,n] = 1 + (((2.0/3.0)* self.time_step * sumRecip) / (1.0 * Consts.RES.value * Consts.CAP.value * avgEdgeLengths))
+
+            for n2 in self.neighbors[n]:
+                tempNeighborRadius = self.neuron.nodes.loc[self.neuron.nodes['node_id'] == n2, 'radius'].values[0] * Consts.SCF.value
+                lhs.at[n,n2] = -1.0*(2.0/3.0) * self.time_step / (1.0 * Consts.RES.value * Consts.CAP.value * tempRadius * avgEdgeLengths * n2 * ((1 / (tempNeighborRadius * tempNeighborRadius)) + (1 / (tempRadius * tempRadius))))
+
+        return lhs, rhs
         
 
     # TODO:
@@ -77,12 +129,14 @@ class Solver:
         self.r = self.r + (((-2.0/3.0) * self.time_step) * self.reactF(self.u[-2], self.n_pre, self.m_pre, self.h_pre))
         self.r = self.r + self.isyn
         self.isyn = self.isyn * 0.0
-        print(self.r)
+        # lhs, rhs = self.makeSparseStencils()
+        # print(lhs)
+        # print(self.r)
         return
 
     
     def print(self):
-        # print(self.neuron.nodes['node_id'].size)
+        print(self.neuron.nodes['node_id'].size)
         print(self.u)
         print(self.m)
         print(self.n)
@@ -94,6 +148,11 @@ class Solver:
         # same as TargetEdgeLength in cs code
         print(self.neuron.sampling_resolution)
         print(self.time_step)
+        print(self.neuron)
+        # print(self.neuron.geodesic_matrix)
+        # for i in range(len(self.neuron.edges)):
+        #     print(self.neuron.geodesic_matrix.at[self.neuron.edges[i][0], self.neuron.edges[i][1]])
+        #     print(self.distances[i])
 
 
 
@@ -138,5 +197,6 @@ if __name__ == "__main__":
     for curStep in range(totalSteps):
         s.solveStep(curStep)
 
+    s.makeSparseStencils()
     print(totalSteps)
 

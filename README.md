e996ccba6cc24e91df613f576dc15f44a24b4f42
Logan Reed <me@loganreed.org>
2024-08-07 01:28:24 -0400

    Finished First Run and Wrote General Guide

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-To convert png outputs to a video
+# Steps
+## Step One
+Add this code to the SolveStep method in SparseSolverTestv1.cs (in Unity Project) at the end of the method, after defining `Upre` but before redefining `U_active`
+```
+using (StreamWriter sw = File.AppendText("/home/occam/Documents/code/neuropy/data/output.txt"))
+{
+sw.WriteLine(String.Join(",",Upre));
+}
+```
+If you need the corresponding swc file, use the method the Neuron class creates to generate one.
+
+## Step Two
+Run the fromUnity.py to create the frames needed to create the non extrapolated version. 
+Select the fps to correspond to desired lag.
+Once the images are generated, convert to video
 ```
 ffmpeg -framerate 30 -i 'outputs/frame%d.png' outputs/out.mp4
 ```
+
+## Step Three
+Edit fromUnity.py to generate extrapolated frames, and use 
+```
+ffmpeg -framerate 30 -i 'outputs/frame%d.png' outputs/out.mp4
+```
+changing the framerate to account for the additional frames
--- neuropy/fromUnity.py
+++ neuropy/fromUnity.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import numpy as np
 from enum import Enum
 import navis as nv
@@ -7,22 +8,59 @@ import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 from scipy.linalg import lu
 
+TIME_STEP = 5e-5
 
-u = pd.read_csv("data/output.txt", delimiter=",", dtype=np.float64)
-print(u)
-n = nv.read_swc("data/test.swc")
-
-i = 0
-for index, row in u.iterrows():
-    if i % 100 == 0:
-        print(index)
-    fig, ax = nv.plot2d(n, linewidth=3, method='2d', view=('x', 'y'), color_by=row.to_numpy(), vmin=-0.010, vmax=0.05, palette="coolwarm")
-    # Save each incremental rotation as frame
-    plt.savefig('outputs/frame{0}.png'.format(i))
-    # for j in range(20):
-    #     plt.savefig('outputs/frame{0}.png'.format(20*i + j))
+def extrapolate(i, j, numExtraps, uCurr, slope, timeStep):
+    uApprox = uCurr + (slope * (timeStep / numExtraps) * j)
+    fig, ax = nv.plot2d(n, linewidth=3, method='2d', view=('x', 'y'), color_by=uApprox, vmin=-0.010, vmax=0.05, palette="coolwarm")
+    plt.savefig('outputs/frame{0}.png'.format(numExtraps*i + j))
     plt.close()
-    i += 1
+
+    return
+
+if __name__ == "__main__":
+    u = pd.read_csv("data/output.txt", delimiter=",", dtype=np.float64)
+    print(u)
+    n = nv.read_swc("data/test.swc")
+
+    i = 0
+    uPrev = 0
+    for index, row in u.iterrows():
+        uCurr = row.to_numpy()
+        if i == 0:
+            uPrev = uCurr
+        if i % 100 == 0:
+            print(index)
+
+        # create approx slope
+        slope = (uCurr - uPrev) / TIME_STEP
+
+        # fig, ax = nv.plot2d(n, linewidth=3, method='2d', view=('x', 'y'), color_by=uCurr, vmin=-0.010, vmax=0.05, palette="coolwarm")
+        # plt.savefig('outputs/frame{0}.png'.format(i))
+        # plt.close()
+
+        numExtraps = 50
+        for j in range(numExtraps):
+            jobs = []
+            p = multiprocessing.Process(target= extrapolate, args = (i, j, numExtraps, uCurr, slope, TIME_STEP))
+            jobs.append(p)
+            p.start()
+        p.join()
+
+
+        # for j in range(30):
+        #     uApprox = uCurr + (slope * TIME_STEP * j)
+        #     fig, ax = nv.plot2d(n, linewidth=3, method='2d', view=('x', 'y'), color_by=uApprox, vmin=-0.010, vmax=0.05, palette="coolwarm")
+        #     plt.savefig('outputs/frame{0}.png'.format(30*i + j))
+        #     plt.close()
+
+        # Save each incremental rotation as frame
+        # for j in range(30):
+        #     plt.savefig('outputs/frame{0}.png'.format(30*i + j))
+        # plt.close()
+        uPrev = row.to_numpy()
+        # print(uPrev)
+        i += 1
 
 
 # fig, ax = nv.plot2d(n, method='2d', view=('x', 'y'), depth_coloring=True)
